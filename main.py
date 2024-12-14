a17a07b6362d24960ee7918f98165f114c2632be
dacidon <thremormagia@gmail.com>
2024-12-14 16:14:10 +0300

    changed some styling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse, FileResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer
 # from db.db_init import Base
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.mount("/static", StaticFiles(directory="ui/static"), name="static")
 
@@ -14,6 +17,10 @@ async def home():
 @app.get("/favicon.ico")
 async def favicon():
     return FileResponse("ui/static/favicon.ico")
+
+@app.get("blog")
+async def blog_handler():
+    return {"articles": ["some", "strange", "bullshit"]}
 
 
 # if __name__ == "main":
